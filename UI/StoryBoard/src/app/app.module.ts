669532b82c6059301e94f6fc31e5b5ef808e1c90
alexandre <alexandrenikitin@outlook.com>
2023-03-20 10:56:22 -0300

    a

--- UI/StoryBoard/src/app/app.module.ts
+++ UI/StoryBoard/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    InitialPageComponent
   ],
   imports: [
     BrowserModule,
