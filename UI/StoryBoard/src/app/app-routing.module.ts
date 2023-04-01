53f4f899c9deea313a1aab620592968f092632e8
Wyllyan <wyll_thibes@hotmail.com>
2023-04-01 14:50:44 -0300

    A

--- UI/StoryBoard/src/app/app-routing.module.ts
+++ UI/StoryBoard/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InitialPageComponent } from './initial-page/initial-page.component';
+import { PresentationPageComponent } from './presentation-page/presentation-page.component';
 
 const routes: Routes = [
   {
     path:'',
+    component: PresentationPageComponent
+  },
+  {
+    path:'initial-page',
     component: InitialPageComponent
   }
 ];
