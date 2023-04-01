dacd32a1133d64f90284a286f35de295d9a8510a
alexandre <alexandrenikitin@outlook.com>
2023-04-01 15:06:15 -0300

    Exemplod de dialog

--- UI/StoryBoard/src/app/app.component.ts
+++ UI/StoryBoard/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'StoryBoard';
   sideNavStatus: boolean = false;
+  public path: string = "";
+
+  constructor(private router : Router) {}
+
+  ngOnInit() {
+    this.path =  window.location.href;
+    console.log(this.path)
+}
 }
